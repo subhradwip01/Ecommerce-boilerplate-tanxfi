43f17b8ce88224973d0f101f625af95ba9a90835
subhradwip01 <subhradwip01@gmail.com>
2024-02-04 17:54:06 +0530

    search featire added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ProductItem from './ProductItem'
 
-const Products = ({products}) => {
+const Products = ({products,query}) => {
     console.log(products)
   return (
     <div className='p-5'>
-        <h1 className="text-[25px] font-bold text-center mb-5">Checkout All <span className='text-rose-400'>Exclusive</span> Products</h1>
+        <h1 className="text-[25px] font-bold text-center mb-5">{query.length==0 ? "Checkout All" : "Searched for"} <span className='text-rose-400'>{query.length === 0 ? "Exclusive" : `"${query}"`}</span> {query.length === 0 && "Products"}</h1>
         <div className='flex flex-wrap justify-center gap-5'>
             {products.map(product=><ProductItem key={product.id} description={product.description} id={product.id} amount={product.amount} title={product.title} image={product.image} rating={product.rating}/> )}
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import Navbar from "../components/Navbar";
 import { getAllProducts } from "../api-service/products";
 import Products from "../components/Products";
 import Loader from "../components/ui/Loader";
+import { Input } from "../components/ui/Input";
 
 const Home = () => {
   const [products, setAllProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
   useEffect(() => {
     const getProducts = async () => {
       setIsLoading(true);
@@ -23,6 +25,10 @@ const Home = () => {
     getProducts();
   }, []);
 
+  const filteredProducts = useMemo(() => {
+    return products.filter((pdt) => pdt.title.includes(query));
+  }, [query]);
+
   return (
     <main className="flex flex-col h-screen">
       <Navbar />
@@ -31,7 +37,20 @@ const Home = () => {
           <Loader />
         </div>
       ) : products.length > 0 ? (
-        <Products products={products} />
+        <>
+          <div className="flex p-5 gap-3">
+            <Input
+              placeholder="Search here..."
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </div>
+          <Products products={filteredProducts} query={query} />
+          {filteredProducts.length ===  0 && (
+            <div className="flex-1 flex items-center justify-center">
+              <h1 className="text-[30px] font-bold">No Search result found</h1>
+            </div>
+          )}
+        </>
       ) : (
         <div className="flex-1 flex items-center justify-center">
           <h1 className="text-[30px] font-bold">
